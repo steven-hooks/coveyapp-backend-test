94b436a10ea3bdc93ba207b9f4ff4bba6b99a4a1
hellosaboteur <hello.saboteur@gmail.com>
2015-01-17 21:52:18 -0500

    Added some tests and comments for dave... to help me out.

--- TestCoveyBackend/src/com/covey/coveyapp/test/TestGeohashZone.java
+++ TestCoveyBackend/src/com/covey/coveyapp/test/TestGeohashZone.java
@@ -25,7 +25,14 @@ public class TestGeohashZone {
 	private static Profile _profileNear;
 	private static Profile _profileOuter;
 	private static Profile _profileFar;
+	private static WGS84Point _closeUpperLeft;
 	
+	/*
+	 * Values for calculating zones:
+	 * Close = .001
+	 * Near = .01
+	 * Outer = .1
+	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		
@@ -33,22 +40,23 @@ public class TestGeohashZone {
 	
 	@Before
 	public void setUp() throws Exception {
-		_profileClose = TestUtilities.makeProfilePositionOnly(40.4595657, -79.9280203);
+		_profileClose = TestUtilities.makeProfilePositionOnly(40.4595657, -79.9280203); //these are the coordinates of cube
 		_boxClose = GeohashZone.getCloseZoneBoundingBox(_profileClose);
-		WGS84Point closeUpperLeft = _boxClose.getUpperLeft();
+		_closeUpperLeft = _boxClose.getUpperLeft();
 		
-		_profileNear = TestUtilities.makeProfilePositionOnly(closeUpperLeft.getLatitude() - 100, closeUpperLeft.getLongitude() + 100);
-		_boxNear = GeohashZone.getNearbyZoneBoundingBox(_profileNear);
+		//this value is not super meaningful, it gets changed a lot by different tests
+		_profileNear = TestUtilities.makeProfilePositionOnly(_closeUpperLeft.getLatitude() + .0000001, _closeUpperLeft.getLongitude() + .001);
 	}	
 	
+	//sanity test - make sure distance is calculated correctly
 	@Test
 	public void testDistanceIsCorrect(){
-		WGS84Point p1 = new WGS84Point(_profileClose.getLatitude(), _profileClose.getLongitude());
+		WGS84Point p1 = new WGS84Point(_profileClose.getLatitude(), _profileClose.getLongitude());		
 		WGS84Point p2 = new WGS84Point(_profileNear.getLatitude(), _profileNear.getLongitude());
 		double distance = TestUtilities.getDistanceBetween2Points(p1, p2);
-		System.out.println("Distance is  "+ distance);
+		System.out.println("DistanceIsCorrect distance = "+ distance);
 		
-		Assert.assertTrue(distance > 0);
+		Assert.assertTrue(distance > .05);
 	}
 
 	@Test
@@ -73,11 +81,17 @@ public class TestGeohashZone {
 
 	@Test
 	public void testIsWithinCloseZoneFalse() {
+		_profileNear = TestUtilities.makeProfilePositionOnly(_closeUpperLeft.getLatitude() + .01, _closeUpperLeft.getLongitude() + .01);
+		System.out.println("WithinCloseFalse distance = "  + TestUtilities.getDistanceBetween2Points(makeWGS(_profileClose), makeWGS(_profileNear)));
 		Assert.assertFalse(GeohashZone.isWithinCloseZone(_profileNear, _profileClose));
 	}
 	
 	@Test
 	public void testIsWithinCloseZoneTrue() {
+		_profileNear = TestUtilities.makeProfilePositionOnly(_closeUpperLeft.getLatitude() + .0000001, _closeUpperLeft.getLongitude() + .001);
+		
+		//DAVE - the distance is 0.0690 this test should pass, yes?
+		System.out.println("WithinCloseTrue distance = "  + TestUtilities.getDistanceBetween2Points(makeWGS(_profileClose), makeWGS(_profileNear)));
 		Assert.assertTrue(GeohashZone.isWithinCloseZone(_profileClose, _profileNear));
 	}
 	
@@ -115,9 +129,12 @@ public class TestGeohashZone {
 		_boxNear = null;
 	}
 	
-	
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+	}
+	
+	private static WGS84Point makeWGS(Profile profile){
+		return new WGS84Point(profile.getLatitude(), profile.getLongitude());
 	}
 	
 }
