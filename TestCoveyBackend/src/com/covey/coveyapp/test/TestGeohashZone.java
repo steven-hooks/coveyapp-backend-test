2413172857ddfbd1220658483f7fb7eb436e1586
steven hooks <steven@coveymom.com>
2015-01-17 20:32:36 -0500

    TestUtilities: Added method to calculate distance. Also added javadoc TestGeohashZone: Added test to verify distance.

--- TestCoveyBackend/src/com/covey/coveyapp/test/TestGeohashZone.java
+++ TestCoveyBackend/src/com/covey/coveyapp/test/TestGeohashZone.java
@@ -33,15 +33,23 @@ public class TestGeohashZone {
 	
 	@Before
 	public void setUp() throws Exception {
-		_profileClose = TestUtilities.makeProfile(40.4595657, -79.9280203);
+		_profileClose = TestUtilities.makeProfilePositionOnly(40.4595657, -79.9280203);
 		_boxClose = GeohashZone.getCloseZoneBoundingBox(_profileClose);
 		WGS84Point closeUpperLeft = _boxClose.getUpperLeft();
 		
-		_profileNear = TestUtilities.makeProfile(closeUpperLeft.getLatitude() - 100, closeUpperLeft.getLongitude() + 100);
+		_profileNear = TestUtilities.makeProfilePositionOnly(closeUpperLeft.getLatitude() - 100, closeUpperLeft.getLongitude() + 100);
 		_boxNear = GeohashZone.getNearbyZoneBoundingBox(_profileNear);
-		
-		System.out.println(distance(_profileClose.getLatitude(), _profileClose.getLongitude(), _profileNear.getLatitude(), _profileClose.getLongitude(), 'K'));
 	}	
+	
+	@Test
+	public void testDistanceIsCorrect(){
+		WGS84Point p1 = new WGS84Point(_profileClose.getLatitude(), _profileClose.getLongitude());
+		WGS84Point p2 = new WGS84Point(_profileNear.getLatitude(), _profileNear.getLongitude());
+		double distance = TestUtilities.getDistanceBetween2Points(p1, p2);
+		System.out.println("Distance is  "+ distance);
+		
+		Assert.assertTrue(distance > 0);
+	}
 
 	@Test
 	public void testGetBestProximity() {
@@ -76,14 +84,16 @@ public class TestGeohashZone {
 	@Test
 	public void testIsWithinCloseZoneP1OnBorder(){
 		WGS84Point boundry = GeohashZone.getCloseZoneBoundingBox(_profileClose).getUpperLeft();
-		_profileNear = TestUtilities.makeProfile(boundry.getLatitude(), boundry.getLongitude());
+		_profileNear = TestUtilities.makeProfilePositionOnly(boundry.getLatitude(), boundry.getLongitude());
+		
 		Assert.assertTrue(GeohashZone.isWithinCloseZone(_profileNear, _profileClose));
 	}
 	
 	@Test
 	public void testIsWithinCloseZoneP2OnBorder(){
 		WGS84Point boundry = GeohashZone.getCloseZoneBoundingBox(_profileClose).getUpperLeft();
-		_profileNear = TestUtilities.makeProfile(boundry.getLatitude(), boundry.getLongitude());
+		_profileNear = TestUtilities.makeProfilePositionOnly(boundry.getLatitude(), boundry.getLongitude());
+		
 		Assert.assertTrue(GeohashZone.isWithinCloseZone(_profileNear, _profileClose));
 	}
 	
@@ -110,35 +120,4 @@ public class TestGeohashZone {
 	public static void tearDownAfterClass() throws Exception {
 	}
 	
-    private static double distance(double lat1, double lon1, double lat2, double lon2, char unit) {
-        double theta = lon1 - lon2;
-        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
-        dist = Math.acos(dist);
-        dist = rad2deg(dist);
-        dist = dist * 60 * 1.1515;
-        if (unit == 'K') {
-          dist = dist * 1.609344;
-        } else if (unit == 'N') {
-          dist = dist * 0.8684;
-          }
-        
-        System.out.println(distance(32.9697, -96.80322, 29.46786, -98.53506, 'M') + " Miles\n");
-        System.out.println(distance(32.9697, -96.80322, 29.46786, -98.53506, 'K') + " Kilometers\n");
-        System.out.println(distance(32.9697, -96.80322, 29.46786, -98.53506, 'N') + " Nautical Miles\n");
-        return (dist);
-      }
-
-      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-      /*::  This function converts decimal degrees to radians             :*/
-      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-      private static double deg2rad(double deg) {
-        return (deg * Math.PI / 180.0);
-      }
-
-      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-      /*::  This function converts radians to decimal degrees             :*/
-      /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-      private static double rad2deg(double rad) {
-        return (rad * 180.0 / Math.PI);
-      }
 }
--- TestCoveyBackend/src/com/covey/coveyapp/test/TestUtilities.java
+++ TestCoveyBackend/src/com/covey/coveyapp/test/TestUtilities.java
@@ -1,19 +1,48 @@
 package com.covey.coveyapp.test;
 
+import ch.hsr.geohash.WGS84Point;
+
 import com.covey.coveyapp.GeohashZone;
 import com.covey.coveyapp.Profile;
+import com.javadocmd.simplelatlng.LatLng;
+import com.javadocmd.simplelatlng.LatLngTool;
+import com.javadocmd.simplelatlng.util.LengthUnit;
 
 public class TestUtilities {
-	
-	//This is to make a profile that only has location data, this will most likely change
-	public static Profile makeProfile(double latitude, double longitude){
+
+	/**
+	 * Returns a profile with only its position (Fields below):
+	 * <ul>
+	 * <li>Latitude</li>
+	 * <li>Longitude</li>
+	 * <li>GeohashLocation</li>
+	 * <li>GeohashZoneCenterLatitude</li>
+	 * <li>GeohashZoneCenterLongitude</li>
+	 * </ul>
+	 * @param latitude
+	 * @param longitude
+	 * @return new Profile
+	 */
+	public static Profile makeProfilePositionOnly(double latitude, double longitude) {
 		Profile profile = new Profile();
 		profile.setLatitude(40.4595657);
-		profile.setLongitude( -79.9280203);
+		profile.setLongitude(-79.9280203);
 		profile.setGeohashLocation(GeohashZone.determineGeohashLocation(profile));
 		profile.setGeohashZoneCenterLatitude(profile.getLatitude());
 		profile.setGeohashZoneCenterLongitude(profile.getLongitude());
-		
+
 		return profile;
+	}
+
+	/**
+	 * Gets the distance, in miles, between 2 points
+	 * @param point1
+	 * @param point2
+	 * @return 
+	 */
+	public static double getDistanceBetween2Points(WGS84Point point1,WGS84Point point2) {
+		LatLng p1 = new LatLng(point1.getLatitude(), point1.getLongitude());
+		LatLng p2 = new LatLng(point2.getLatitude(), point2.getLongitude());
+		return LatLngTool.distance(p1, p2, LengthUnit.MILE);
 	}
 }
